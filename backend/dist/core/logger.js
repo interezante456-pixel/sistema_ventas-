9af0cf9a2ae078eb72b2bb5ebea6e40af29d9081
Interezante456 <interezante456@gmail.com>
2025-12-14 21:32:20 -0500

    chore: delete file

--- backend/dist/core/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-exports.logger = {
-    info: (message) => console.log(`[INFO] ${new Date().toISOString()} - ${message}`),
-    error: (message, error) => console.error(`[ERROR] ${new Date().toISOString()} - ${message}`, error),
-    warn: (message) => console.warn(`[WARN] ${new Date().toISOString()} - ${message}`),
-};
